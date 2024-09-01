61b47b09a208a10d0ba5076bfefef0f5f7af61e1
younboyoung <byyoun@osci.kr>
2024-09-01 17:12:13 +0900

    frontend Login 화면 구성

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,40 +1,168 @@
 import React, { useState } from 'react';
-import '../style/Login.css';
+import {
+    Button,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    TextField,
+    InputAdornment,
+    Link,
+    IconButton,
+} from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { AppProvider, SignInPage } from '@toolpad/core';
+import { useTheme } from '@mui/material/styles';
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
-function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+const providers = [{ id: 'credentials', name: 'Email and Password' }];
 
-    const handleSubmit = (event) => {
+function CustomEmailField() {
+    return (
+        <TextField
+            id="input-with-icon-textfield"
+            label="Username"
+            name="email"
+            type="email"
+            size="small"
+            required
+            fullWidth
+            slotProps={{
+                input: {
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <AccountCircle fontSize="inherit" />
+                        </InputAdornment>
+                    ),
+                },
+            }}
+            variant="outlined"
+        />
+    );
+}
+
+function CustomPasswordField() {
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
         event.preventDefault();
-        // 로그인 처리 로직
     };
 
     return (
-        <div className="login-container">
-            <form onSubmit={handleSubmit}>
-                <h2>Login</h2>
-                <div className="form-group">
-                    <label>Username</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+        <FormControl sx={{ my: 2 }} fullWidth variant="outlined">
+            <InputLabel size="small" htmlFor="outlined-adornment-password">
+                Password
+            </InputLabel>
+            <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                size="small"
+                endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            size="small"
+                        >
+                            {showPassword ? (
+                                <VisibilityOff fontSize="inherit" />
+                            ) : (
+                                <Visibility fontSize="inherit" />
+                            )}
+                        </IconButton>
+                    </InputAdornment>
+                }
+                label="Password"
+            />
+        </FormControl>
+    );
+}
+
+function CustomButton() {
+    return (
+        <Button
+            type="submit"
+            variant="outlined"
+            color="info"
+            size="small"
+            disableElevation
+            fullWidth
+            sx={{ my: 2 }}
+        >
+            Sign In
+        </Button>
+    );
+}
+
+function SignUpLink() {
+    return (
+        <Link href="/" variant="body2">
+            Sign up
+        </Link>
+    );
+}
+
+function ForgotPasswordLink() {
+    return (
+        <Link href="/" variant="body2">
+            Forgot password?
+        </Link>
+    );
+}
+
+function Login() {
+    const theme = useTheme();
+
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleLoginSubmit = async (provider, formData) => {
+        // const username = formData.get('email');
+        const username = "admin";
+        const password = formData.get('password');
+
+        try {
+            const response = await axios.post('http://localhost:8080/api/login',
+                new URLSearchParams({
+                    username,
+                    password
+                }), {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Access-Control-Allow-Credentials': true
+                    },
+                    withCredentials: true
+                });
+
+            if (response.status === 200) {
+                navigate('/home'); // 로그인 성공 시 home으로 리다이렉트
+            }
+        } catch (error) {
+            setError('Invalid username or password');
+        }
+    }
+
+    return (
+        <AppProvider theme={theme}>
+            <SignInPage
+                signIn={(provider, formData) => handleLoginSubmit(provider, formData)}
+                slots={{
+                    emailField: CustomEmailField,
+                    passwordField: CustomPasswordField,
+                    submitButton: CustomButton,
+                    signUpLink: SignUpLink,
+                    forgotPasswordLink: ForgotPasswordLink,
+                }}
+                providers={providers}
+            />
+        </AppProvider>
     );
 }
 
